1af7c8901f0d007792f10c55592c8ead47e98338
Michelyserras <Michelysrrs@gmail.com>
2024-10-31 13:18:19 -0300

    exercicio 1

---
+++ Ex1/Ex1.java
@@ -0,0 +1,16 @@
+
+public class Ex1 {
+
+    public static void main(String[] args){
+        int indice = 13, k = 0, soma = 0;
+
+        do{
+            k = k+1;
+            soma = soma + k;
+            System.out.println(soma);
+        } while(k < indice);
+       
+
+    }
+
+}
