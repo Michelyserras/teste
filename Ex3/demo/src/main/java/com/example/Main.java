87f7ea334faf7ab607da203e0c366b074334855b
Michelyserras <Michelysrrs@gmail.com>
2024-11-07 16:48:25 -0300

    imprimindo maior e menor valor e quantidade de valores acima da media mensal

--- Ex3/demo/src/main/java/com/example/Main.java
+++ Ex3/demo/src/main/java/com/example/Main.java
@@ -10,15 +10,15 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         
-        Gson gson = new Gson();
+        Gson gson = new Gson(); // instanciando a biblioteca para conseguir acessar os metodos
         
-       try (FileReader reader = new FileReader("Ex3\\demo\\src\\main\\resources\\dados.json")) {
+       try (FileReader reader = new FileReader("Ex3\\demo\\src\\main\\resources\\dados.json")) { // utilizando filereader para ler o arquivo json dentro do projeto
        
-        Type listType = new TypeToken<List<Registro>>() {}.getType();
+        Type listType = new TypeToken<List<Registro>>() {}.getType(); // transformando esse json em uma lista do tipo registro
 
-        List<Registro>  registros = gson.fromJson(reader, listType);
+        List<Registro>  registros = gson.fromJson(reader, listType); // em seguida cria-se uma lista do tipo registro para armazenar todos os dados da lista json
         
-        registros.forEach(registro ->{
+        registros.forEach(registro ->{ // em segida imprimimos na tela cada registro da lista
             System.out.println("Dia: " + registro.getDia() + "\nValor: " + registro.getValor() + "\n");
         });
         } catch (IOException e) {
