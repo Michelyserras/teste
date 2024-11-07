52d8aca86d05b81077992a07eba3ff7278b04c36
Michelyserras <Michelysrrs@gmail.com>
2024-11-07 17:07:45 -0300

    ex 4 percentuais

--- Ex4/Ex4/src/App.java
+++ Ex4/Ex4/src/App.java
@@ -5,10 +5,10 @@ public class App {
         double MG = 29229.88;
         double ES = 27165.48;
         double Outros = 19849.53;
-        double soma = 0;
 
-        soma = SP + RJ + MG + ES + Outros;
+        double soma = SP + RJ + MG + ES + Outros;
 
+        System.out.println("Total Faturamento = " + soma);
         System.out.println("Percentual SP = " + (SP/soma)*100 + "%\n");
         System.out.println("Percentual RJ = " + (RJ/soma)*100 + "%\n");
         System.out.println("Percentual Mg = " + (MG/soma)*100 + "%\n");
